c64c3d3753fe009835b35d01bb5b3534405dd34a
Ryan Niemeyer <rniemeyer@gmail.com>
2015-05-20 14:02:50 -0500

    Update version to 0.5.2

--- build/knockout-postbox.js
+++ build/knockout-postbox.js
@@ -1,4 +1,4 @@
-// knockout-postbox 0.5.1 | (c) 2015 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
+// knockout-postbox 0.5.2 | (c) 2015 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
 ;(function(factory) {
     //CommonJS
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
