48696cd0582bc683945ad97e309e4ff70e7ff76b
Greg Kubisa <gkubisa@gmail.com>
2017-03-30 15:44:11 +0100

    Bump the version

--- build/knockout-postbox.js
+++ build/knockout-postbox.js
@@ -1,4 +1,4 @@
-// knockout-postbox 0.5.2 | (c) 2017 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
+// knockout-postbox 0.5.3 | (c) 2017 Ryan Niemeyer |  http://www.opensource.org/licenses/mit-license
 ;(function(factory) {
     //CommonJS
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
